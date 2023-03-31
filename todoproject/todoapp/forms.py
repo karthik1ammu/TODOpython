3fe3f22dd0991b86f21645324cd6da27e41ea17d
karthik1ammu <amruthapp41@gmail>
2023-03-31 15:02:09 +0530

    added

---
+++ todoproject/todoapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from todoapp.models import Task
+
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model=Task
+        fields=['name','priority','date']
+
+
